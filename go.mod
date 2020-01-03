8c1ec7c766f4410cdb512934a4ae1b926470dea4
chenchao <wenbushi@gmail.com>
2020-01-03 13:23:52 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chen-chao/transcribe
+
+go 1.13
---
+++ transcribe.go
@@ -0,0 +1,59 @@
+package transcribe
+
+import (
+	"reflect"
+)
+
+// Transcribe copy src in any type to an interface
+func Transcribe(src interface{}) interface{} {
+	v := reflect.ValueOf(src)
+	return copyAny(v).Interface()
+}
+
+func copyAny(v reflect.Value) reflect.Value {
+	switch v.Kind() {
+	case reflect.Array, reflect.Slice:
+		return copySlice(v)
+	case reflect.Struct:
+		return copyStruct(v)
+	case reflect.Map:
+		return copyMap(v)
+	case reflect.Ptr:
+		return copyPtr(v)
+	case reflect.Interface:
+		return copyInterface(v)
+	default:
+		return v
+	}
+}
+
+func copySlice(v reflect.Value) reflect.Value {
+	d := reflect.MakeSlice(v.Type(), v.Len(), v.Cap())
+	for i := 0; i < v.Len(); i++ {
+		d.Index(i).Set(copyAny(v.Index(i)))
+	}
+	return d
+}
+
+func copyStruct(v reflect.Value) reflect.Value {
+	d := reflect.New(v.Type()).Elem()
+	// TODO: the mutable field will change along with the original
+	d.Set(v)
+	return d
+}
+
+func copyMap(v reflect.Value) reflect.Value {
+	d := reflect.MakeMap(v.Type())
+	for _, key := range v.MapKeys() {
+		d.SetMapIndex(key, copyAny(v.MapIndex(key)))
+	}
+	return d
+}
+
+func copyPtr(v reflect.Value) reflect.Value {
+	return copyAny(v.Elem()).Addr()
+}
+
+func copyInterface(v reflect.Value) reflect.Value {
+	return copyAny(v.Elem())
+}
